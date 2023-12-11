d5047cf17bccb3e0246bba4136f34737886f9050
vincetyy <vince.tanyy@gmail.com>
2023-12-11 16:19:54 +0800

    done 4.17

---
+++ part4/blog/controllers/users.js
@@ -0,0 +1,37 @@
+const bcrypt = require('bcrypt')
+const usersRouter = require('express').Router()
+const User = require('../models/user.js')
+
+usersRouter.post('/', async (request, response) => {
+  const { username, name, password } = request.body
+
+  if (username === undefined || username.length < 3) {
+    return response.status(400).json({ error: 'expected `username` to be at least 3 characters' })
+  }
+
+  if (password === undefined || password.length < 3) {
+    return response.status(400).json({ error: 'expected `password` to be at least 3 characters' })
+  }
+
+  const saltRounds = 10
+  const passwordHash = await bcrypt.hash(password, saltRounds)
+
+  const user = new User({
+    username,
+    name,
+    passwordHash,
+  })
+
+  const savedUser = await user.save()
+
+  response.status(201).json(savedUser)
+})
+
+usersRouter.get('/', async (request, response) => {
+  const users = await User
+    .find({})
+    .populate('blogs', { 'title': 1, 'author': 1, 'id': 1})
+  response.json(users)
+})
+
+module.exports = usersRouter
