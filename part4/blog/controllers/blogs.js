21f95b6aea85cd394e7915577073764707a62534
vincetyy <vince.tanyy@gmail.com>
2023-12-12 00:51:48 +0800

    done 4.22

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -1,7 +1,7 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
-const User = require('../models/user')
-const jwt = require('jsonwebtoken')
+
+
 
 
 blogsRouter.get('/', async (request, response) => {
@@ -23,12 +23,7 @@ blogsRouter.get('/:id', async (request, response) => {
 blogsRouter.post('/',  async (request, response) => {
   const body = request.body
 
-  const decodedToken = jwt.verify(request.token, process.env.SECRET)
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: 'token invalid'})
-  }
-
-  const user = await User.findById(decodedToken.id)
+  const user = request.user
 
   const blog = new Blog ({
     title: body.title,
@@ -47,12 +42,7 @@ blogsRouter.post('/',  async (request, response) => {
 blogsRouter.delete('/:id',  async (request, response) => {
   const blogToDelete = await Blog.findById(request.params.id)
 
-  const decodedToken = jwt.verify(request.token, process.env.SECRET)
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: 'token invalid'})
-  }
-
-  const user = await User.findById(decodedToken.id)
+  const user = request.user
 
   if (blogToDelete.user.toString() !== user.id.toString()) {
     return response.status(401).json({ error: 'invalid user'})
--- part4/blog/utils/middleware.js
+++ part4/blog/utils/middleware.js
@@ -1,4 +1,6 @@
 const logger = require('./logger')
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
 
 const requestLogger = (request, response, next) => {
   logger.info('Method:', request.method)
@@ -15,6 +17,16 @@ const tokenExtractor = (request, response, next) => {
   } else {
     request.token = null
   }
+  next()
+}
+
+const userExtractor = async (request, response, next) => {
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid'})
+  }
+
+  request.user = await User.findById(decodedToken.id)
   next()
 }
 
@@ -42,6 +54,7 @@ const errorHandler = (error, request, response, next) => {
 module.exports = {
   requestLogger,
   tokenExtractor,
+  userExtractor,
   unknownEndpoint,
   errorHandler
 }
